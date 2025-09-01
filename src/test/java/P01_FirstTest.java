00215c7528d31f3aef0cb04883d30bb1b2741fc7
ElifDemKa <elif.demircikav@gmail.com>
2025-09-01 13:41:46 +0300

    first commit

---
+++ src/test/java/P01_FirstTest.java
@@ -0,0 +1,18 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+public class P01_FirstTest {
+    public static void main(String[] args) {
+        try (Playwright playwright= Playwright.create()) {
+        Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));//istenilen browser ismi kullanılır
+        // pencereyi acıp açmaması -true false- ile saglanabilir
+        Page page= browser.newPage(); // Seleniumdaki eşdegeri driver
+        page.navigate("https://www.testotomasyonu.com");
+        System.out.println(page.title()+ "\n" +page.url());
+        //Not: Bu sekilde calistirildiginda pencere acılır fakat kapanmadıgından create ederken try eklenir
+
+}
+    }
+}
---
+++ src/test/java/P02_FirstTest.java
@@ -0,0 +1,33 @@
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.awt.*;
+
+public class P02_FirstTest {
+    public static void main(String[] args) throws InterruptedException {
+        Dimension screenSize=Toolkit.getDefaultToolkit().getScreenSize();
+        int width=screenSize.width;
+        int height= screenSize.height;
+        Playwright playwright= Playwright.create();
+        Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(true));
+        //setHeadless(true):hic pencere acmadan testi tamamlar
+        //setHeadless(false):browser acarak testi tamamlar
+        Page page= browser.newPage();
+        page.navigate("https://www.babayigit.net/test");
+        System.out.println(page.title());
+        page.setViewportSize(width,height);
+        Thread.sleep(15000);
+
+        page.close();
+        browser.close();
+        playwright.close();
+        //bu sekilde de acilan pencere kapatılabilir
+
+
+
+
+
+    }
+}
