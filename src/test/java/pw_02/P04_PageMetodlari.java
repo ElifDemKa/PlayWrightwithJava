7980701d9c3bfb03bb00396304b3e1cc057ccc92
ElifDemKa <elif.demircikav@gmail.com>
2025-11-29 21:20:04 +0300

    pw02

---
+++ src/test/java/pw_02/P04_PageMetodlari.java
@@ -0,0 +1,35 @@
+package pw_02;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.awt.*;
+
+public class P04_PageMetodlari {
+    public static void main(String[] args) throws InterruptedException {
+        Dimension screenSize= Toolkit.getDefaultToolkit().getScreenSize();
+        int width=screenSize.width;
+        int height= screenSize.height;
+        Playwright playwright=Playwright.create();
+        Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false).setSlowMo(10)); //setslowmo testi yavaşlatır explity gerek kalmaz
+        Page page= browser.newPage();
+
+        page.navigate("https://www.testotomasyonu.com");
+        page.fill("//*[@class='search-label']","iphone");
+        page.keyboard().press("Enter");
+        page.hover("//*[@class='lazy']");
+        Thread.sleep(3000);
+        page.waitForLoadState();
+        System.out.println(page.title());
+
+
+
+        page.close();
+        browser.close();
+        playwright.close();
+
+
+    }
+}
---
+++ src/test/java/pw_02/P06_PageSelect.java
@@ -0,0 +1,28 @@
+package pw_02;
+
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
+
+import java.awt.*;
+
+public class P06_PageSelect {
+    public static void main(String[] args) throws InterruptedException {
+        Dimension scrSize=Toolkit.getDefaultToolkit().getScreenSize();
+        int width= scrSize.width;
+        int height= scrSize.height;
+        Playwright playwright=Playwright.create();
+        Browser browser=playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page= browser.newPage();
+        page.setViewportSize(width,height);
+        page.navigate("https://testautomationpractice.blogspot.com/");
+        page.selectOption("//*[@id='country']","japan");
+        Thread.sleep(3500);
+
+        page.close();
+        browser.close();playwright.close();
+
+
+    }
+}
