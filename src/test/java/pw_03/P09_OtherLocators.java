1363cfdeceeeb68d2b4f5be55b32cfb66d0c2a27
ElifDemKa <elif.demircikav@gmail.com>
2025-12-03 20:45:54 +0300

    pw03

--- src/test/java/pw_03/P09_OtherLocators.java
+++ src/test/java/pw_03/P09_OtherLocators.java
@@ -4,6 +4,9 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserType;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.Playwright;
+import com.microsoft.playwright.options.LoadState;
+
+import java.awt.*;
 
 public class P09_OtherLocators {
     public static void main(String[] args) {
@@ -11,6 +14,14 @@ public class P09_OtherLocators {
         Playwright playwright = Playwright.create();
         Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
         Page page = browser.newPage();
+
+        Dimension scr=Toolkit.getDefaultToolkit().getScreenSize();
+        int width= scr.width;
+        int height=scr.height;
+        page.setViewportSize(width,height);
+        page.navigate("https://getir.com");
+        System.out.println(page.title()+"\n");
+        page.waitForLoadState(LoadState.LOAD);
 
     }
 }
