095c4e9caa50a58c7fa3ff031c9253f50406224f
ElifDemKa <elif.demircikav@gmail.com>
2025-12-03 20:44:12 +0300

    pw03

--- src/test/java/pw_03/P09_OtherLocators.java
+++ src/test/java/pw_03/P09_OtherLocators.java
@@ -1,5 +1,16 @@
 package pw_03;
 
-public class P09_OtherLocators {
+import com.microsoft.playwright.Browser;
+import com.microsoft.playwright.BrowserType;
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.Playwright;
 
+public class P09_OtherLocators {
+    public static void main(String[] args) {
+
+        Playwright playwright = Playwright.create();
+        Browser browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
+        Page page = browser.newPage();
+
+    }
 }
