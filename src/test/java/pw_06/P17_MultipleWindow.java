35fab8565132831cf21c6eb5b7683881bcc81475
ElifDemKa <elif.demircikav@gmail.com>
2025-12-09 22:03:20 +0300

    p06

--- src/test/java/pw_06/P17_MultipleWindow.java
+++ src/test/java/pw_06/P17_MultipleWindow.java
@@ -1,10 +1,29 @@
 package pw_06;
 
+import com.microsoft.playwright.Page;
+import com.microsoft.playwright.options.LoadState;
+import org.junit.jupiter.api.Test;
 import utilities.BaseTest;
+
+import java.util.List;
 
 public class P17_MultipleWindow extends BaseTest { // birden fazla pencere acmak istersek
 
+    @Test
+    public void tets(){
+        page.navigate("https://demoqa.com/browser-windows"); //adrese git
+        page.waitForLoadState(LoadState.LOAD);
 
+        Page popup=page.waitForPopup(new Page.WaitForPopupOptions().setPredicate(
+                p-> p.context().pages().size()==2),()->{page.getByText("New Window").first().click();});
+        // bir tane poup pencersi olustur ve sayfa acıla kadar bekle
+
+        List<Page> pages=popup.context().pages(); //acılan sayfa liste yazdır
+        System.out.println(popup.title());
+        System.out.println(page.title());
+        System.out.println(pages.size());
+
+    }
 
 
 
---
+++ src/test/java/pw_06/P18_ScreenShot.java
@@ -0,0 +1,46 @@
+package pw_06;
+
+import com.microsoft.playwright.Locator;
+import com.microsoft.playwright.Page;
+import org.junit.jupiter.api.Test;
+import utilities.BaseTest;
+
+import java.nio.file.Paths;
+
+public class P18_ScreenShot extends BaseTest {
+    @Test
+    public void screenshotTest() throws InterruptedException {
+        page.navigate("https://getir.com/");
+        Thread.sleep(2000);
+
+
+        //aAcılan kısmın fotografını alma
+        String dosyaYolu= "src/test/java/utilities/1.png" ;
+        String dosyaYolu2= "src/test/java/utilities/2.png" ;
+        String dosyaYolu3= "src/test/java/utilities/3.png" ;
+
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(dosyaYolu)).setFullPage(true)); // sayfanın tamamını ekran goruntusu al
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(dosyaYolu2)));
+        page.screenshot(new Page.ScreenshotOptions().setPath(Paths.get(dosyaYolu3)));// sayfanın sadece gorunen kısmının e.goruntusunu al
+
+        Locator logo = page.locator("img[alt='Logo']");//LOGONUN FOTOGRAFINI AL
+        //logo.screenshot(new Locator.ScreenshotOptions().setPath(Paths.get(dosyaYolu3)));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
